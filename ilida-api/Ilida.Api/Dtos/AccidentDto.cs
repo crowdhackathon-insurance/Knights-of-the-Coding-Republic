4495404e4d8f11220e2d4d4b3972cd56e4d8ff17
Theofanis Tokas <tokastheofanis@gmail.com>
2016-10-02 10:31:00 +0300

    Add more dtos in API

---
+++ ilida-api/Ilida.Api/Dtos/AccidentDto.cs
@@ -0,0 +1,39 @@
+﻿using Ilida.Api.Models;
+using System;
+using System.Collections.Generic;
+
+namespace Ilida.Api.Dtos
+{
+    public class AccidentDto
+    {
+        public long Id { get; set; }
+
+        public DateTime OccuredOn { get; set; }
+
+        public bool HasInjuries { get; set; }
+
+        public bool HasOtherVehicleDamages { get; set; }
+
+        public bool HasOtherItemsDamages { get; set; }
+
+        public double Latitude { get; set; }
+
+        public double Longitude { get; set; }
+
+        public string DiagramUrl { get; set; }
+
+        public WorkflowStatus WorkflowStatus { get; set; }
+
+        public ICollection<AccidentParticipant> AccidentParticipants { get; set; }
+
+        public ICollection<AccidentCarDto> AccidentCars { get; set; }
+
+        public ICollection<AccidentPhotoDto> AccidentPhotos { get; set; }
+
+        public ICollection<Company> AccidentCompanies { get; set; }
+
+        public ICollection<AccidentWitnessDto> AccidentWitnesses { get; set; }
+
+        public ICollection<AccidentActionDto> AccidentActions { get; set; }
+    }
+}
