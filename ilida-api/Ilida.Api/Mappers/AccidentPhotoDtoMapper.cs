614b4e39da4bfe2d6b902ffc90e23f5e0bf47f28
Theofanis Tokas <tokastheofanis@gmail.com>
2016-10-02 10:31:16 +0300

    Add more dto mappers in API

---
+++ ilida-api/Ilida.Api/Mappers/AccidentPhotoDtoMapper.cs
@@ -0,0 +1,19 @@
+﻿using Ilida.Api.Dtos;
+using Ilida.Api.Models;
+
+namespace Ilida.Api.Mappers
+{
+    public class AccidentPhotoDtoMapper : BaseMapper<AccidentPhoto, AccidentPhotoDto>
+    {
+        public override AccidentPhotoDto Map(AccidentPhoto photo)
+        {
+            if (photo == null) return null;
+
+            return new AccidentPhotoDto
+            {
+                Id = photo.Id,
+                Url = photo.Url
+            };
+        }
+    }
+}
