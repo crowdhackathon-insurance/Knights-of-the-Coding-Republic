73d6ae9f96666e425284cffbc4b2ae0508a775c9
hampos <hampos@me.com>
2016-10-02 11:18:02 +0300

    bootstrap ilida expert app

---
+++ ilida-mobile-expert/ilida.mobile.expert/ilida.mobile.expert/ViewModels/AccidentListViewModel.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using Xamarin.Forms;
+
+namespace ilida.mobile.expert
+{
+	public class AccidentListViewModel : BaseViewModel
+	{
+		INavigationService _nav;
+
+		public AccidentListViewModel(INavigationService nav)
+		{
+			_nav = nav;
+			_accidents = new List<Accident>()
+			{
+				new Accident(){ AccidentId="891291", Date="11/09/2016 14:30", Status="Προς Διακανονισμο"},
+				new Accident(){ AccidentId="732291", Date="12/03/2015 11:00", Status="Ολοκληρώθηκε"}
+			};
+			SubmitCommand = new Command(async () => await Submit());
+			SelectCommand = new Command<Accident>(async (a) => await Select(a));
+		}
+
+		public ICommand SubmitCommand { get; set; }
+		public ICommand SelectCommand { get; set; }
+
+		private ICollection<Accident> _accidents;
+		public ICollection<Accident> Accidents
+		{
+			get
+			{
+				return _accidents;
+			}
+			set
+			{
+				_accidents = value;
+				OnPropertyChanged(nameof(Accidents));
+			}
+		}
+
+		private Accident _selectedItem;
+		public Accident SelectedItem
+		{
+			get
+			{
+				return _selectedItem;
+			}
+			set
+			{
+				_selectedItem = value;
+				if (_selectedItem == null)
+				{
+					return;
+				}
+				SelectCommand.Execute(_selectedItem);
+				_selectedItem = null;
+				OnPropertyChanged(nameof(SelectedItem));
+			}
+		}
+
+		public async Task Submit()
+		{
+			//await _nav.PushAsync<SubmitAccidentViewModel>();
+		}
+
+		public async Task Select(Accident a)
+		{
+			//var avm = new AccidentViewModel(_nav)
+			//{
+			//	AccidentId = a.AccidentId,
+			//	Status = a.Status
+			//};
+			//await _nav.PushAsync(avm);
+		}
+
+	}
+}
---
+++ ilida-mobile-expert/ilida.mobile.expert/ilida.mobile.expert/Views/AccidentListView.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace ilida.mobile.expert
+{
+	public partial class AccidentListView : ContentPage
+	{
+		public AccidentListView()
+		{
+			InitializeComponent();
+		}
+	}
+}
