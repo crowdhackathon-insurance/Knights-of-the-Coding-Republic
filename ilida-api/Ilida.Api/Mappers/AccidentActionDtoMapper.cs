ec674f34de2a3183c06fbacda73f79a5945d2128
Theofanis Tokas <tokastheofanis@gmail.com>
2016-10-02 10:39:17 +0300

    Add more dto mappers

---
+++ ilida-api/Ilida.Api/Mappers/AccidentActionDtoMapper.cs
@@ -0,0 +1,23 @@
+﻿using Ilida.Api.Dtos;
+using Ilida.Api.Models;
+
+namespace Ilida.Api.Mappers
+{
+    public class AccidentActionDtoMapper : BaseMapper<AccidentAction, AccidentActionDto>
+    {
+        public override AccidentActionDto Map(AccidentAction action)
+        {
+            if (action == null) return null;
+
+            return new AccidentActionDto
+            {
+                Id = action.Id,
+                Timestamp = action.Timestamp,
+                Comment = action.Comment,
+                WorkflowAction = action.WorkflowAction,
+                PreviousWorkflowStatusId = action.PreviousWorkflowStatusId,
+                NextWorkflowStatusId = action.NextWorkflowStatusId
+            };
+        }
+    }
+}
